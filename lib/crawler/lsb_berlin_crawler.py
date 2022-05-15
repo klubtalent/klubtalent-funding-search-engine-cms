39138a5ceca11b1b65b6a78729874cd29bb0edc3
Florian Schwanz <florian.schwanz@interoberlin.de>
2022-05-16 00:47:47 +0200

    fix: typo

--- lib/crawler/lsb_berlin_crawler.py
+++ lib/crawler/lsb_berlin_crawler.py
@@ -200,10 +200,10 @@ def generate_content(logger, results_path, funding):
         values["region"] = funding.region
     if len(funding.category) > 0:
         values["category"] = funding.category.title().replace("Und", "und").replace("Des", "des")
-    if funding.volume > 0:
-        values["volume"] = funding.volume
     if len(funding.updated) > 0:
         values["updated"] = funding.updated
+
+    values["volume"] = funding.volume
 
     if len(funding.sports) > 0:
         for s in funding.sports:
